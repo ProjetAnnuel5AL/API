1df31f098a45d18e65b8ef950dd3d510d3b1060f
iPlowPlow <plowplow2011@hotmail.fr>
2018-06-05 18:50:07 +0200

    merge new repo

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 module.exports = function(app, models, TokenUtils) {
 
-	app.get('/', function (req, res) {
-		res.send('Hello on API !');
-	});
-
 	require("./user")(app, models, TokenUtils);
 	require("./unit")(app, models);
 	require("./category")(app, models);
 	require("./producer")(app, models, TokenUtils);
-	require("./item")(app, models);
+	require("./item")(app, models, TokenUtils);
 	require("./product")(app, models);
 
 };
